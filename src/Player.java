1895404ac4f5b3ca8d5cef67622fac87d6308a7d
BT_1N3_10 <sdsdbhjsd@nsdj.com>
2024-03-15 08:21:45 -0400

    monsterfighting method in player class

--- src/Player.java
+++ src/Player.java
@@ -1,8 +1,9 @@
+// push pls
+
 public class Player extends Space {
     private String name;
     private int health;
     private Item item;
-
     public Player(String name) {
         super(name.substring(0, 1).toUpperCase()); // symbol is first initial
         this.name = name;
