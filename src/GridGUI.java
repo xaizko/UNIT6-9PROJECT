859402624355ccd88c7e09c7c8fd61f382fbb840
shifat <Shifatulg@gmail.com>
2024-04-06 15:50:18 -0400

    Working on bosses

--- src/GridGUI.java
+++ src/GridGUI.java
@@ -8,14 +8,23 @@ import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.util.ArrayList;
 public class GridGUI {
+    // frame
+    private JFrame frame;
+    // sea and attributes
+    private JPanel sea;
     private JLabel player;
     private JLabel coin;
-    private JLabel merchant;
-    private JFrame frame;
+    private JLabel skeleton;
+    private JLabel octopus;
+    private JLabel ghost;
+    private JLabel gameTheory;
+    // mob fight
     private JPanel mobFight;
+    // boss fight and attributes
     private JPanel bossFight;
-    private JPanel sea;
+    // shop and attributes
     private JPanel shop;
+    private JLabel merchant;
     private JTextPane balance;
     private JTextPane welcome;
     private ArrayList<Item> inventory;
@@ -30,9 +39,12 @@ public class GridGUI {
         player = new JLabel(new ImageIcon("src/ImagePirate.png"));
         coin = new JLabel(new ImageIcon("src/ImageShop.png"));
         merchant = new JLabel(new ImageIcon("src/ImageMerchant.png"));
+        skeleton = new JLabel(new ImageIcon("src/ImageSkeleton.png"));
+        octopus = new JLabel();
+        ghost = new JLabel();
+        gameTheory = new JLabel();
         frame = new JFrame("Pirate Cove");
         mobFight = new JPanel();
-        bossFight = new JPanel();
         sea = new JPanel();
         shop = new JPanel();
         attributes = new Player("Traveler");
@@ -43,11 +55,9 @@ public class GridGUI {
         frame.setSize(1000,1000);
         play();
     }
-
     private void play() throws UnsupportedAudioFileException, LineUnavailableException, IOException, InterruptedException {
 //        AudioPlayer mainTheme = new AudioPlayer("Main Theme Pirates of the Caribbean.wav");
 //        mainTheme.playSound();
-
         frame.add(sea);
         frame.setVisible(true);
         while (!gameOver) {
@@ -59,18 +69,37 @@ public class GridGUI {
                 frame.add(shop);
                 frame.setVisible(true);
             }
+            if (player.getX() == skeleton.getX() && player.getY() == skeleton.getY()) {
+                player.setLocation(450,450);
+                fightBoss(ethiron, new JLabel(new ImageIcon("src/ImageSans.png")), Color.BLUE);
+            }
+            if (player.getX() == octopus.getX() && player.getY() == octopus.getY()) {
+                player.setLocation(450,450);
+                fightBoss(cthyllus, new JLabel(new ImageIcon("src/ImageCthulhu(Cthyllus).png")), Color.WHITE);
+            }
+            if (player.getX() == ghost.getX() && player.getY() == ghost.getY()) {
+                player.setLocation(450,450);
+                fightBoss(daveyJones, new JLabel(new ImageIcon("src/ImageDaveyJones.png")), Color.MAGENTA);
+            }
+            if (player.getX() == gameTheory.getX() && player.getY() == gameTheory.getY()) {
+                player.setLocation(450,450);
+                fightBoss(matPat, new JLabel(new ImageIcon("src/ImageMatPat.png")), Color.GREEN);
+            }
         }
+    }
+    private void fightBoss(Boss boss, JLabel img, Color color) throws UnsupportedAudioFileException, LineUnavailableException, IOException {
+        boss.encounterBoss();
+        frame.setVisible(false);
+        frame.remove(sea);
+        bossFight = new JPanel();
+        bossFight.setBackground(color);
+        bossFight.setSize(1000,1000);
+        bossFight.add(img);
+        frame.add(bossFight);
+        frame.setVisible(true);
     }
 
     private void initializeSea() {
-        shop.setLayout(null);
-        shop.setSize(1000,1000);
-        shop.setBackground(Color.LIGHT_GRAY);
-
-        merchant.setBounds(100,0,500,500);
-
-        shop.add(merchant);
-
         sea.setLayout(null);
         sea.setSize(1000,1000);
         sea.setBackground(Color.CYAN);
@@ -79,12 +108,22 @@ public class GridGUI {
 
         coin.setBounds(500,500,50,50);
 
+        skeleton.setBounds(450,0,50,50);
+
         makeGrid(sea, 0);
+
         sea.add(player);
         sea.add(coin);
+        sea.add(skeleton);
     }
 
     private void initializeShop() {
+        shop.setLayout(null);
+        shop.setSize(1000,1000);
+        shop.setBackground(Color.LIGHT_GRAY);
+
+        merchant.setBounds(100,0,500,500);
+
         welcome = new JTextPane();
         welcome.setText("Welcome to the Merchant, what would you like to buy");
         welcome.setBackground(Color.LIGHT_GRAY);
@@ -147,7 +186,7 @@ public class GridGUI {
                 frame.setVisible(true);
             }
         } );
-
+        shop.add(merchant);
         shop.add(balance);
         shop.add(welcome);
         shop.add(lifeCrystal);
