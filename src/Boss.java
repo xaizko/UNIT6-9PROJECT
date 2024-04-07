075d81d182c334f0f6231063c2a5ec577d39aaa2
shifat <Shifatulg@gmail.com>
2024-04-07 10:59:58 -0400

    Refractored file names so it makes more sense

--- src/Boss.java
+++ src/Boss.java
@@ -18,16 +18,16 @@ public class Boss extends Monster {
     }
     public void playMusic() throws UnsupportedAudioFileException, LineUnavailableException, IOException {
         if(type == 1){
-            audioPlayer = new AudioFile("Undertale - Megalovania.wav");
+            audioPlayer = new AudioFile("ethiron'sCalling.wav");
             audioPlayer.playSound();
         }else if(type == 2){
-            audioPlayer = new AudioFile("If You Need The Most Epic Dark Boss Theme, Check This • ＂CTHULHU AWAKENS＂ by Apollon de Moura.wav");
+            audioPlayer = new AudioFile("cthyllusAwakens.wav");
             audioPlayer.playSound();
         } else if (type == 3) {
-            audioPlayer = new AudioFile("Norway Theme - Atomic (Civilization 6 OST) ｜ Gjendines Bånlåt.wav");
+            audioPlayer = new AudioFile("daveyJonesLocker.wav");
             audioPlayer.playSound();
         }else if(type == 4){
-            audioPlayer = new AudioFile("Science Blaster (Song Remix created by Nirre & Acid Usagi) Game Theory Audio Spectrum Tribute.wav");
+            audioPlayer = new AudioFile("matPat'sConcierto.wav");
             audioPlayer.playSound();
         }else {
             audioPlayer = new AudioFile("Two Steps from Hell - Heart of Courage.wav");
